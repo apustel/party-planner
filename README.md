7c8257b25a1af881aae7965bf3d4d5e87f23c1d1
apustel <apusteln@gmail.com>
2024-11-07 15:34:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AWS Training
+## CloudFormation
+
+Party Planner
---
+++ registration/app.py
@@ -0,0 +1,40 @@
+import boto3
+import os
+import json
+
+
+def lambda_handler(event, context):
+    parties_table = os.getenv('PARTIES_TABLE')
+    guests_table = os.getenv('GUESTS_TABLE')
+    dynamo_client = boto3.client('dynamodb')
+    print(event)
+    body = json.loads(event['body'])
+    resp = dynamo_client.get_item(
+        TableName=parties_table,
+        Key={'PartyName': {'S': body['PartyName']}}
+    )
+    if 'Item' not in resp:
+        dynamo_client.put_item(
+            TableName=parties_table,
+            Item={
+                'PartyName': {'S': body['PartyName']},
+                'Date': {'S': body['PartyDate']}
+            }
+        )
+    dynamo_client.put_item(
+        TableName=guests_table,
+        Item={
+            'GuestName':{'S': body['GuestName']},
+            'Diet': {'S': body['GuestDiet']},
+            'PartyName':{'S': body['PartyName']}
+        }
+    )
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',  # Dieser Header erlaubt CORS
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS, GET'  # HTTP-Methoden angeben
+        }
+    }
+
